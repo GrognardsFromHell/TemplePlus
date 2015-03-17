32e8b8d99157398072d2b7529051a3210ba4e3f5
Sebastian Hartte <sebastian@hartte.de>
2015-03-17 01:45:53 +0100

    Added python and an example for adding more python scripting

---
+++ python.cpp
@@ -0,0 +1,38 @@
+
+#include "stdafx.h"
+#include "fixes.h"
+#include "addresses.h"
+#include "dependencies/python-2.2/Python.h"
+
+/*
+	Define all type objects used by ToEE.
+*/
+static GlobalStruct<PyTypeObject, 0x102CF3B8> pyObjHandleType;
+static getattrfunc pyObjHandleTypeGetAttr; // Original getattr of pyObjHandleType
+
+PyObject* __cdecl  pyObjHandleType_getAttrNew(PyObject *obj, char *name) {
+	
+	LOG(info) << "Tried getting property: " << name;
+	if (!strcmp(name, "co8rocks")) {
+		return PyString_FromString("IT SURE DOES!");
+	}
+
+	return pyObjHandleTypeGetAttr(obj, name);
+}
+
+class PythonExtensions : public TempleFix {
+public:
+	const char* name() override {
+		return "Python Script Extensions";
+	}
+	void apply() override;
+} pythonExtension;
+
+void PythonExtensions::apply() {
+
+	// Hook the getattr function of obj handles
+	pyObjHandleTypeGetAttr = pyObjHandleType->tp_getattr;
+	pyObjHandleType->tp_getattr = pyObjHandleType_getAttrNew;
+
+
+}
