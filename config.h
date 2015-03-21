c4dccefa7cd5ea3f6f0da6e4f634222819c17736
DudeMcDude <shai.sha2304@gmail.com>
2015-03-21 23:16:03 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	TemplePlus.vcxproj
    	TemplePlus.vcxproj.filters
    	python.cpp
    	temple_functions.h
    
    XP Table for High Levels + Debug toggles + Tidying up

--- config.h
+++ config.h
@@ -3,11 +3,18 @@
 
 struct TemplePlusConfig
 {
-	bool skipIntro = false;
-	bool skipLegal = false;
+	bool skipIntro = true;
+	bool skipLegal = true;
 	bool useDirect3d9Ex = true;
 	bool windowed = true;
+
+	// debug msgs
 	bool featPrereqWarnings = false;
+	bool spellAlreadyKnownWarnings = false;
+	
+	// gameplay
+	double randomEncounterExperienceFactor = 0.7; // an additional factor; e.g. if the normal Experience Multiplier is 0.7 and this is 0.7, overall is 0.49
+	bool NPCsLevelLikePCs = true;
 };
 
 extern TemplePlusConfig config;
---
+++ xp.h
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+const uint32_t MAXLEVEL = 20;
+const int CRMIN = -2; // equiv to CR 1/4  (next ones are CR 1/3, CR 1/2, CR 1, CR 2, CR 3,...
+const int CRMAX = 20;
+
+class XPAward{
+public:
+	int XPAwardTable[20][23];
+	XPAward();
+};
