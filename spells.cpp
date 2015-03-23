cca5597fea22a165b54eceac2a7dac2ec4920c86
DudeMcDude <shai.sha2304@gmail.com>
2015-03-23 02:32:55 +0200

    Hostility Bug Fix

--- spells.cpp
+++ spells.cpp
@@ -2,7 +2,7 @@
 #include "stdafx.h"
 #include "fixes.h"
 
-/*
+
 class SpellHostilityFlagFix : public TempleFix {
 public:
 	const char* name() override {
@@ -10,10 +10,9 @@ public:
 	}
 
 	void apply() override {
-		//LOG(info) << "Applying Spell Hostility (Cure X Wounds, Mass) Bug fix...";
-		//writeHex(0x10076EF4, "02"); // Cure Light Wounds, Mass
-		//writeHex(0x10076F42, "02"); // Mass Heal
-		//writeHex(0x10077058, "02 02 02"); // Cure Moderate + Serious + Critical Wounds, Mass
+		LOG(info) << "Applying Spell Hostility (Cure X Wounds, Mass) Bug fix...";
+		writeHex(0x10076EF4, "02"); // Cure Light Wounds, Mass
+		writeHex(0x10076F42, "02"); // Mass Heal
+		writeHex(0x10077058, "02 02 02"); // Cure Moderate + Serious + Critical Wounds, Mass
 	}
 } spellHostilityFlagFix;
-*/
