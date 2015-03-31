7a9e2412442bc207e65adf5abe90bf81a755ce2c
DudeMcDude <shai.sha2304@gmail.com>
2015-03-31 20:07:40 +0300

    Kukri as Martial Weapon partial fix

--- generalfixes.cpp
+++ generalfixes.cpp
@@ -14,3 +14,15 @@ public:
 		writeHex(0x10278078, "73 69 7A 65 5F 63 6F 6C 6F 73 73 61 6C");
 	}
 } sizeColossalFix;
+
+class KukriFix : public TempleFix {
+public:
+	const char* name() override {
+		return "Makes Kukri Proficiency Martial";
+	}
+
+	void apply() override {
+		writeHex(0x102BFD78 + 30*4, "00 09"); // marks Kukri as Martial in the sense that picking "Martial Weapons Proficiency" will now list Kukri
+		// TODO: makes the feat "Martial Weapons Proficiency: All" include Kukri proficiency
+	}
+} kukriFix;
