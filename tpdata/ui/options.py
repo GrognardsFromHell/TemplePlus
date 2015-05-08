7643e1d7310ad824e35e4c889f074853a5cf2a9f
Sebastian Hartte <sebastian@hartte.de>
2015-05-08 22:07:37 +0200

    Tabs look like Tabs now.

--- tpdata/ui/options.py
+++ tpdata/ui/options.py
@@ -4,3 +4,14 @@ def accept():
 
 def decline():
     pass
+
+#
+# librocket does not support inline blocks with width: auto
+# so we emulate that by hand here
+#
+def resizetabs(doc):
+    for tab in doc.GetElementsByTagName("tab"):
+        spans = tab.GetElementsByTagName("span")
+        if len(spans) == 1:
+            w = int(spans[0].client_width)
+            tab.style.width = "%dpx" % (w, )
