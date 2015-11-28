3e5edb8051d4ce0b0f0dba8f63e06f1a58bc26cb
Sebastian Hartte <sebastian@hartte.de>
2015-11-28 21:18:13 +0100

    Fixed geometry shadows.

--- Temple/src/aasrenderer.cpp
+++ Temple/src/aasrenderer.cpp
@@ -111,6 +111,7 @@ Material AasRenderer::CreateGeometryShadowMaterial(gfx::RenderingDevice &device)
 	RasterizerState rasterizerState;
 	rasterizerState.cullMode = D3DCULL_NONE;
 	DepthStencilState depthStencilState;
+	depthStencilState.depthEnable = false;
 
 	auto vs{ device.GetShaders().LoadVertexShader("shadow_geom_vs") };
 	auto ps{ device.GetShaders().LoadPixelShader("diffuse_only_ps") };
