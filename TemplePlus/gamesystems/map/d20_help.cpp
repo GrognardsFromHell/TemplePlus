80a023cd46aea4455f57d5544f7ecf8f6cec770c
DudeMcDude <shai.sha2304@gmail.com>
2015-11-28 21:45:55 +0200

    Fixed help_extensions.tab terminator character for last line

--- TemplePlus/gamesystems/map/d20_help.cpp
+++ TemplePlus/gamesystems/map/d20_help.cpp
@@ -5,11 +5,17 @@
 #include <hashtable.h>
 #include <infrastructure/elfhash.h>
 #include <tio/tio.h>
+#include <tig/tig_tokenizer.h>
 
 
 HelpSystem helpSys;
+
+ToEEHashtableSystem<D20HelpTopic> d20HelpHashTableSys;
+
 int(__cdecl*HelpSystem::orgGenerateLinks)(D20HelpTopic * d20ht);
 int(__cdecl*HelpSystem::orgLinkParser)(D20HelpLink* d20hl, char * topicTitle, char **pos1, char **pos2, int *offsetOut);
+
+int _HelpSystemTabLineParserPreliminary(const TigTabParser * tabParser, int lineIdx, char ** columns);
 
 struct HelpSystemAddresses : temple::AddressTable
 {
@@ -55,14 +61,14 @@ int HelpSystem::HelpTabInit()
 	logger->info("Parsing Help Data...");
 
 	TigTabParser tabOrg;
-	tigTabParserFuncs.Init(&tabOrg, addresses.HelpSystemTabLineParserPreliminary);
+	tigTabParserFuncs.Init(&tabOrg, _HelpSystemTabLineParserPreliminary);
 	tigTabParserFuncs.Open(&tabOrg, "mes\\help.tab");
 	int helpTabNumLines = tigTabParserFuncs.GetLineCount(&tabOrg);
 
 
 	// the extensions
 	TigTabParser tabExt;
-	tigTabParserFuncs.Init(&tabExt, addresses.HelpSystemTabLineParserPreliminary);
+	tigTabParserFuncs.Init(&tabExt, _HelpSystemTabLineParserPreliminary);
 	tigTabParserFuncs.Open(&tabExt, "tpmes\\help_extensions.tab");
 	int helpExtNumLines = tigTabParserFuncs.GetLineCount(&tabExt);
 	
@@ -133,3 +139,49 @@ int HelpSystem::LinkParser(D20HelpLink* d20hl, char* topicTitle, char** pos1, ch
 	int result = orgLinkParser(d20hl, topicTitle, pos1, pos2, offsetOut);
 	return result;
 }
+
+
+
+int _HelpSystemTabLineParserPreliminary(const TigTabParser * tabParser, int lineIdx, char ** columns)
+{
+	int topicId = ElfHash::Hash(columns[0]);
+	auto d20ht = new D20HelpTopic;
+	d20ht->topicId = topicId;
+	if (*columns[1])
+		d20ht->parentId = ElfHash::Hash(columns[1]);
+	else
+		d20ht->parentId = *addresses.hashTAG_ROOT;
+
+	if (*columns[2])
+		d20ht->prevId = ElfHash::Hash(columns[2]);
+	else
+		d20ht->prevId = *addresses.hashTAG_ROOT;
+
+	d20ht->siblingId = 0;
+	d20ht->nextId = 0;
+	d20ht->vParentsSize = 0;
+	d20ht->virtualParents = nullptr;
+	StringTokenizer helpTok(columns[3]);
+	while(helpTok.next())
+	{
+		if (helpTok.token().type == StringTokenType::Identifier)
+		{
+			
+			d20ht->virtualParents = static_cast<int*>(realloc(d20ht->virtualParents, 4 * d20ht->vParentsSize + 4));
+			if (! d20ht->virtualParents)
+			{
+				logger->error("D20HelpTopic: unable to alloc memory to hold help table. Exiting...");
+				exit(0);
+			}
+			d20ht->virtualParents[d20ht->vParentsSize++] = ElfHash::Hash(helpTok.token().text);
+		}
+	}
+	d20ht->vChildrenSize = 0;
+	d20ht->virtualChildren = nullptr;
+	d20ht->title = (char*)malloc(strlen(columns[4]) + 1);
+	strcpy(d20ht->title , columns[4]);
+
+	d20HelpHashTableSys.HashtableAddItem(addresses.helpSysHashTable, d20ht->topicId, d20ht);
+
+	return 0;
+};
