4b1b5c632786ba0937ff8e306d0162a76ade6d0e
DMD <hedetet@gmail.com>
2017-04-10 11:44:47 +0300

    Fix for object clone

--- TemplePlus/gamesystems/objects/objfind.cpp
+++ TemplePlus/gamesystems/objects/objfind.cpp
@@ -6,5 +6,9 @@
 void ObjFindSupport::Remove(objHndl handle, GameObjectBody * obj)
 {
 	static auto obj_find_remove = temple::GetPointer<void(objHndl)>(0x100c11f0);
+
+	auto findNode = obj->GetInt32(obj_f_find_node);
+	if (!findNode)
+		return;
 	obj_find_remove(handle);
 }
