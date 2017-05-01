86ebc895fb4f2f6e5c9182ad17d5083faa466bb3
DMD <hedetet@gmail.com>
2017-05-01 13:59:05 +0300

    Fixed bards getting free weapons due to manual proficiency in their entry

--- TemplePlus/weapon.cpp
+++ TemplePlus/weapon.cpp
@@ -36,65 +36,42 @@ uint32_t WeaponSystem::IsExotic(WeaponTypes wpnType)
 
 uint32_t WeaponSystem::IsDruidWeapon(WeaponTypes wpnType)
 {
-	switch (wpnType)
-	{
-	case wt_dagger:
-		return 1;
-	case wt_sickle:
-		return 1;
-	case wt_club:
-		return 1;
-	case wt_shortspear:
-		return 1;
-	case wt_quarterstaff:
-		return 1;
-	case wt_spear:
-		return 1;
-	case wt_dart:
-		return 1;
-	case wt_sling:
-		return 1;
-	case wt_scimitar:
-		return 1;
-	case wt_longspear:
-		return 1;
-	default:
-		return 0;
+	switch (wpnType){
+		case wt_dagger:
+		case wt_sickle:
+		case wt_club:
+		case wt_shortspear:
+		case wt_quarterstaff:
+		case wt_spear:
+		case wt_dart:
+		case wt_sling:
+		case wt_scimitar:
+		case wt_longspear:
+			return 1;
+		default:
+			return 0;
 	}
 	return 0;
 }
 
 
-uint32_t WeaponSystem::IsMonkWeapon(WeaponTypes wpnType)
-{
-	switch (wpnType)
-	{
-	case wt_dagger:
-		return 1;
-	case wt_club:
-		return 1;
-	case wt_quarterstaff:
-		return 1;
-	case wt_light_crossbow:
-		return 1;
-	case wt_sling:
-		return 1;
-	case wt_heavy_crossbow:
-		return 1;
-	case wt_javelin:
-		return 1;
-	case wt_handaxe:
-		return 1;
-	case wt_kama:
-		return 1;
-	case wt_nunchaku:
-		return 1;
-	case wt_siangham:
-		return 1;
-	case wt_shuriken:
-		return 1;
-	default:
-		return 0;
+uint32_t WeaponSystem::IsMonkWeapon(WeaponTypes wpnType){
+	switch (wpnType){
+		case wt_dagger:
+		case wt_club:
+		case wt_quarterstaff:
+		case wt_light_crossbow:
+		case wt_sling:
+		case wt_heavy_crossbow:
+		case wt_javelin:
+		case wt_handaxe:
+		case wt_kama:
+		case wt_nunchaku:
+		case wt_siangham:
+		case wt_shuriken:
+			return 1;
+		default:
+			return 0;
 	}
 	return 0;
 }
@@ -104,18 +81,12 @@ uint32_t WeaponSystem::IsRogueWeapon(uint32_t wielderSize, WeaponTypes wpnType)
 {
 	// TODO: looks like Troika intended to differentiate by the Wielder's Size? Was not implemented
 	if (weapons.IsSimple(wpnType)){ return 1; }
-	switch (wpnType)
-	{
+	switch (wpnType){
 	case wt_hand_crossbow:
-		return 1;
 	case wt_rapier:
-		return 1;
 	case wt_short_sword:
-		return 1;
 	case wt_sap:
-		return 1;
 	case wt_shortbow:
-		return 1;
 	case wt_composite_shortbow:
 		return 1;
 	default:
@@ -127,20 +98,15 @@ uint32_t WeaponSystem::IsRogueWeapon(uint32_t wielderSize, WeaponTypes wpnType)
 
 uint32_t WeaponSystem::IsWizardWeapon(WeaponTypes wpnType)
 {
-	switch (wpnType)
-	{
-	case wt_dagger:
-		return 1;
-	case wt_club:
-		return 1;
-	case wt_quarterstaff:
-		return 1;
-	case wt_light_crossbow:
-		return 1;
-	case wt_heavy_crossbow:
-		return 1;
-	default:
-		return 0;
+	switch (wpnType){
+		case wt_dagger:
+		case wt_club:
+		case wt_quarterstaff:
+		case wt_light_crossbow:
+		case wt_heavy_crossbow:
+			return 1;
+		default:
+			return 0;
 	}
 	return 0;
 }
@@ -150,15 +116,10 @@ uint32_t WeaponSystem::IsElvenWeapon(WeaponTypes wpnType)
 	switch (wpnType)
 	{
 	case wt_longsword:
-		return 1;
 	case wt_rapier:
-		return 1;
 	case wt_shortbow:
-		return 1;
 	case wt_composite_shortbow:
-		return 1;
 	case wt_longbow:
-		return 1;
 	case wt_composite_longbow:
 		return 1;
 	default:
@@ -176,17 +137,11 @@ uint32_t WeaponSystem::IsBardWeapon(WeaponTypes wpnType)
 	switch (wpnType)
 	{
 	case wt_sap:
-		return 1;
 	case wt_short_sword:
-		return 1;
 	case wt_longsword:
-		return 1;
 	case wt_rapier:
-		return 1;
 	case wt_shortbow:
-		return 1;
 	case wt_composite_shortbow:
-		return 1;
 	case wt_whip:
 		return 1;
 	default:
@@ -195,10 +150,9 @@ uint32_t WeaponSystem::IsBardWeapon(WeaponTypes wpnType)
 	return 0;
 }
 
-bool WeaponSystem::IsSlashingOrBludgeoning(objHndl weapon)
-{
+bool WeaponSystem::IsSlashingOrBludgeoning(objHndl weapon){
 	if (!weapon)
-		return 0;
+		return false;
 	auto weaponType = objects.GetWeaponType(weapon);
 	return IsSlashingOrBludgeoning(weaponType);
 }
