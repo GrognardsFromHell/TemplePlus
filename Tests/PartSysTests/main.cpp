f912ea151eb99438b2c81cdecaa1c96bb579fa12
Sebastian Hartte <sebastian@hartte.de>
2017-12-25 00:37:08 +0100

    Removed Google Test (moved to dependencies)

--- Tests/PartSysTests/main.cpp
+++ Tests/PartSysTests/main.cpp
@@ -2,10 +2,25 @@
 #include "stdafx.h"
 #include <windows.h>
 
+#include <experimental/filesystem>
+using namespace std::experimental::filesystem::v1;
+
+static void ChangeToDataDirectory() {
+	path p = current_path();
+
+	while (p.has_parent_path()) {
+		if (exists(p / "TemplePlus.sln")) {
+			current_path(p / "Tests" / "PartSysTests");
+			return;
+		}
+
+		p = p.parent_path();
+	}
+}
+
 int main(int argc, char **argv)
 {
-	TCHAR path[MAX_PATH];
-	GetCurrentDirectory(MAX_PATH, path);
+	ChangeToDataDirectory();
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
