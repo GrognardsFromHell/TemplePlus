42369393e1634441346a63baecf00b6dc9551c01
IPDTFE <11908071+IPDTFE@users.noreply.github.com>
2018-02-18 00:34:43 +0100

    Create vexing_flanker.py

---
+++ tpdatasrc/tpgamefiles/scr/tpModifiers/vexing_flanker.py
@@ -0,0 +1,21 @@
+#Vexing Flanker:  Player's Handbook II, p. 85
+
+from templeplus.pymod import PythonModifier
+from toee import *
+import tpdp
+
+print "Registering Vexing Flanker"
+
+def VFing(attachee, args, evt_obj):
+
+	if attachee.has_feat("Vexing Flanker") != 0:
+		#Vexing Flanker
+		target = evt_obj.attack_packet.target
+		if evt_obj.attack_packet.get_flags() & D20CAF_FLANKED:
+			evt_obj.bonus_list.add(2, 0, "Target Vexing flanker bonus")
+
+	return 0
+
+eVF = PythonModifier("Vexing Flanker Feat", 2)
+eVF.MapToFeat("Vexing Flanker")
+eVF.AddHook(ET_OnToHitBonus2, EK_NONE, VFing, ())
