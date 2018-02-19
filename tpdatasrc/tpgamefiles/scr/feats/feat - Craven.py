afe1b9f09840a5f2322ffecad4e7bf44877db32b
IPDTFE <11908071+IPDTFE@users.noreply.github.com>
2018-02-19 14:36:14 +0100

    Update feat - Craven.py

--- tpdatasrc/tpgamefiles/scr/feats/feat - Craven.py
+++ tpdatasrc/tpgamefiles/scr/feats/feat - Craven.py
@@ -6,4 +6,6 @@ def CheckPrereq(attachee, classLevelled, abilityScoreRaised):
 	#Sneak Attack Check
 	if not attachee.has_feat(feat_sneak_attack):
 		return 0
+	if attachee.stat_level_get(stat_level_paladin) > 1: #workaround until I figure out how to check for immunity to fear
+		return 0
 	return 1
