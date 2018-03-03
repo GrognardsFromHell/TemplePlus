4a6e0339772a007ff706845598e711d5ab6f7861
doug1234 <doug1234@unknown>
2018-03-02 22:53:54 -0500

    Now using highest_divine_caster_level for caster level checking.

--- tpdatasrc/tpgamefiles/scr/feats/feat - Divine Armor.py
+++ tpdatasrc/tpgamefiles/scr/feats/feat - Divine Armor.py
@@ -6,18 +6,9 @@ def CheckPrereq(attachee, classLevelled, abilityScoreRaised):
 	#Req 1, turn undead feat
 	if not (attachee.has_feat(feat_turn_undead) or attachee.has_feat(feat_rebuke_undead)):
 		return 0
-		
-	#Req2, Divine Caster Level 5 (calculate caster rather than calling function to avoid item/feat bonuses)
-	paladinCasterLevel = attachee.stat_level_get(stat_caster_level_paladin)
-	if paladinCasterLevel >= 5:
-		return 1
-		
-	clericCasterLevel = attachee.stat_level_get(stat_caster_level_cleric)
-	if clericCasterLevel >= 5:
-		return 1
 	
-	blackGuardCasterLevel = attachee.stat_level_get(stat_caster_level, stat_level_blackguard)
-	if blackGuardCasterLevel >= 5:
+	#Req2, Divine Caster Level 5
+	if attachee.highest_divine_caster_level >= 5:
 		return 1
 		
 	return 0
