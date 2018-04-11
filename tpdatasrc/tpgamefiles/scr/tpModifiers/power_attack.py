12c92c7f06d9618c7d6a4c954e28b451c0d31f69
doug1234 <doug1234@unknown>
2018-04-10 22:22:40 -0400

    Added the ability to query power attack value.

---
+++ tpdatasrc/tpgamefiles/scr/tpModifiers/power_attack.py
@@ -0,0 +1,13 @@
+from templeplus.pymod import PythonModifier
+from toee import *
+import tpdp
+
+print "Registering Power Attack Extender"
+
+def PowerAttackValue(attachee, args, evt_obj):
+	evt_obj.return_val = args.get_arg(0)
+	return 0
+
+powerAttackExtender = PythonModifier()
+powerAttackExtender.ExtendExisting("Power Attack")
+powerAttackExtender.AddHook(ET_OnD20PythonQuery, "Power Attack Value", PowerAttackValue, ())
