ee5dccdb635f1d2fa8524553b3d8a9445e5b990d
Dude McDude <DudeMcDude@users.noreply.github.com>
2018-04-16 18:28:22 +0300

    Update feat - Favored Power Attack.py
    Updated to use the new char_editor facilities

--- tpdatasrc/tpgamefiles/scr/feats/feat - Favored Power Attack.py
+++ tpdatasrc/tpgamefiles/scr/feats/feat - Favored Power Attack.py
@@ -1,5 +1,5 @@
 from toee import *
-
+import char_editor
 
 def CheckPrereq(attachee, classLevelled, abilityScoreRaised):
 
@@ -7,12 +7,12 @@ def CheckPrereq(attachee, classLevelled, abilityScoreRaised):
 	if attachee.get_base_attack_bonus() < 4:
 		return 0
 	
-	if attachee.has_feat(feat_power_attack) == 0:
+	if char_editor.has_feat(feat_power_attack) == 0:
 		return 0
 	
 	#Check for any favored enemy feat
 	for i in range (feat_favored_enemy_aberration , feat_favored_enemy_humanoid_human):
-		if attachee.has_feat(i):
+		if char_editor.has_feat(i):
 			return 1
 	
 	return 0
