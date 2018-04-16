a0ddd329cfb8d00a85e2713f36c0ca7c513aed6d
Dude McDude <DudeMcDude@users.noreply.github.com>
2018-04-16 18:29:08 +0300

    Update feat - Telling Blow.py

--- tpdatasrc/tpgamefiles/scr/feats/feat - Telling Blow.py
+++ tpdatasrc/tpgamefiles/scr/feats/feat - Telling Blow.py
@@ -1,9 +1,9 @@
 from toee import *
-
+import char_editor
 
 def CheckPrereq(attachee, classLevelled, abilityScoreRaised):
 
-	if attachee.has_feat(feat_sneak_attack):
+	if char_editor.has_feat(feat_sneak_attack):
 		return 1
 	
 	return 0
