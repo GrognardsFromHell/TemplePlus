7e4b14d6b2ebc340c952d1658f0a1dec6e1c08ad
IPDTFE <11908071+IPDTFE@users.noreply.github.com>
2018-08-19 16:35:04 +0200

    Delete EncounterPackTest.py

--- tpdatasrc/co8fixes/scr/EncounterPackTest.py
+++
@@ -1,9 +0,0 @@
-from random_encounter import *
-
-
-def EncounterExpander(setup, encounter):
-	list = []
-	list.append(RE_entry( 11,    ( (14957, 1, 2, 1),  ),      1028))  #Test fight with atropal scions
-	return list
-
-
