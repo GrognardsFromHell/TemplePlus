eeb57f4569260ad6e434e69ded501edc45ee927d
IPDTFE <11908071+IPDTFE@users.noreply.github.com>
2018-09-02 15:01:07 +0200

    Update expand_encounters.py

--- tpdatasrc/tpgamefiles/scr/expand_encounters.py
+++ tpdatasrc/tpgamefiles/scr/expand_encounters.py
@@ -1,11 +1,11 @@
 import os
-import random
+import sys
 from toee import *
 #name convention for encounterpacks is EncounterPack + Creator name + pack number
 #the file must contain a function called EncounterExpander which takes 2 parameters and returns a list of RE_expanded to be added
 
-
-encounterpacks = os.listdir('.\overrides\scr')	#needs to be changed, not sure which folder should contain these files
+sys.path.insert(0, '.\encounterpacks')
+encounterpacks = os.listdir('.\encounterpacks')	#to install a new encounterpack simply create a folder called encounterpacks in your main TOEE folder and save the file there
 
 class RE_expanded:
 	# Description:
@@ -47,38 +47,49 @@ class RE_expanded:
 
 	
 def encounter_coordinator( setup, encounter ):
-	if((1000 <= encounter.id <= 1999 ) or encounter.id == 4000):	#is it a random encounter? or is it a sleep encounter? if it's neither, don't touch it!
+	if((1000 <= encounter.id <= 1999 ) or encounter.id == 4000):
 		extra_encounters(setup, encounter)											
-	return														
+	return
 		
 def extra_encounters(setup, encounter):
 	expanded_list = []
+	nobe = 8			#average number of base encounters, in vanilla TOEE the average number of encounters per map is 8.375, rounded down to 8
+	if game.global_flags[500] == 1 and encounter.id == 4000:
+		nobe = 5		#average number of sleep encounters for vanilla TOEE, rounded up from 4,81
+	elif game.global_flags[500] == 1:
+		nobe = 14		#average number of random encounters for Co8 NC TOEE, rounded up from 13,75
+	elif encounter.id == 4000:
+		nobe = 6		#average number of sleep encounters for Co8 NC TOEE rounded up from 5.642
+	
+						#the average number of encounters per sleep area varies greatly, it could be improved with an area by area number, defaulting to the average if not found
 	
 	for f in encounterpacks:
-		#print (f)						#testing purposes only, to check what files are available in the opened folder
+		print (f)
 		if f.startswith('EncounterPack'):																	#name convention is EncounterPack + Creator name + pack number
 			expanded_list.extend((__import__(os.path.splitext(f)[0]).EncounterExpander(setup, encounter)))	#search encounterpacks and receive returned encounters	            
 																											#encounter and setup provided for customization
 																											
-	if(game.random_range(1,len(expanded_list ) + 14) <= len(expanded_list)):						#weighted chance to find a new encounter instead of a base one	
-																									#NC base encounters average to 13,75~, rounded to 14	
-
-		aaa = game.random_range(0, len(expanded_list) -1)											#it's time to determine encounter dc, id, enemies, location (player spawnpoint)
+	if(len (expanded_list) > 0 and game.random_range(1,len(expanded_list ) + nobe) <= len(expanded_list)):	#weighted chance to find a new encounter instead of a base one, since this code is only called when
+																					#the game found a normal random encounter, here we make a random check, with a weighted chance that 
+																					#depends on how	many encounters we added and how many base encounters there are. This way even though
+																					#we added new encounters, there will be a fair chance for all encounters, base and extra, to appear.
+		
+		aaa = game.random_range(0, len(expanded_list) -1)											#NC base encounters average to 13,75~, rounded to 14	
 		encounter.enemies = expanded_list[aaa].get_enemies()
-		encounter.dc = expanded_list[aaa].dc														
+		encounter.dc = expanded_list[aaa].dc													#encounter dc, id, enemies, location (player spawnpoint)
 
-		if(encounter.id != 4000):								#not a sleep encounter, take a map from those available for the encounter and its id, then randomize a location
-			if(len(expanded_list[aaa].maplist) > 0):												
+		if(encounter.id != 4000):
+			if(len(expanded_list[aaa].maplist) > 0):
 				encounter.map = expanded_list[aaa].maplist[game.random_range(0, len(expanded_list[aaa].maplist) -1)]
 			randomLocation(encounter)
 			encounter.id = expanded_list[aaa].id
 
-		elif(encounter.id == 4000):								#it's a sleep encounter! we shouldn't change the map
+		elif(encounter.id == 4000):
 			encounter.map = game.leader.map
 
 	return
 
-def randomLocation(encounter):									#taken from Co8 version of random_encounter.py, seems to work fine
+def randomLocation(encounter):
 	r = game.random_range(1,3)
 	if (r == 1):
 		encounter.location = location_from_axis( 470, 480 )
