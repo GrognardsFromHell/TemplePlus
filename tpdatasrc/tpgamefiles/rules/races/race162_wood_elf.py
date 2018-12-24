2726df840b48787f05e6ad90ea74b140594bf5bd
doug1234 <da2628@gmail.com>
2018-12-23 21:21:25 -0500

    Added 4 new elven subraces.

---
+++ tpdatasrc/tpgamefiles/rules/races/race162_wood_elf.py
@@ -0,0 +1,33 @@
+from toee import *
+import race_defs
+
+###################################################
+print "Registering race: Wood Elf"
+
+raceEnum = race_elf + (5 << 5)
+
+raceSpec = race_defs.RaceSpec()
+raceSpec.hit_dice        = dice_new("0d0")
+raceSpec.level_modifier  = 0
+raceSpec.help_topic      = "TAG_WOOD_ELF"
+raceSpec.flags           = 0
+raceSpec.modifier_name   = "Wood Elf"
+raceSpec.height_male     = [53, 65]
+raceSpec.height_female   = [53, 65]
+raceSpec.weight_male     = [87, 121]
+raceSpec.weight_female   = [82, 116]
+raceSpec.stat_modifiers  = [2, 2, -2, -2, 0, -2]   # str, dex, con, int, wis, cha
+raceSpec.proto_id        = 13004
+raceSpec.material_offset = 2         # offset into rules/material_ext.mes file
+raceSpec.feats           = [feat_simple_weapon_proficiency_elf]
+
+###################################################
+def RegisterRace():
+	raceSpec.register(raceEnum)
+
+def GetFavoredClass(obj = OBJ_HANDLE_NULL):
+	return stat_level_ranger
+
+def GetLevelModifier(obj = OBJ_HANDLE_NULL):
+	return 0
+
