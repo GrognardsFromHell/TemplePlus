be20d121d53934947d2682bc81ea5716481d1383
doug1234 <da2628@gmail.com>
2018-12-26 21:57:03 -0500

    Added help and prototype entries.

--- tpdatasrc/tpgamefiles/rules/races/race130_wild_elf.py
+++ tpdatasrc/tpgamefiles/rules/races/race130_wild_elf.py
@@ -17,7 +17,7 @@ raceSpec.height_female   = [53, 65]
 raceSpec.weight_male     = [87, 121]
 raceSpec.weight_female   = [82, 116]
 raceSpec.stat_modifiers  = [0, 2, 0, -2, 0, 0]   # str, dex, con, int, wis, cha
-raceSpec.proto_id        = 13004
+raceSpec.proto_id        = 13028
 raceSpec.material_offset = 2         # offset into rules/material_ext.mes file
 raceSpec.feats           = [feat_simple_weapon_proficiency_elf]
 
--- tpdatasrc/tpgamefiles/rules/races/race162_wood_elf.py
+++ tpdatasrc/tpgamefiles/rules/races/race162_wood_elf.py
@@ -17,7 +17,7 @@ raceSpec.height_female   = [53, 65]
 raceSpec.weight_male     = [87, 121]
 raceSpec.weight_female   = [82, 116]
 raceSpec.stat_modifiers  = [2, 2, -2, -2, 0, -2]   # str, dex, con, int, wis, cha
-raceSpec.proto_id        = 13004
+raceSpec.proto_id        = 13030
 raceSpec.material_offset = 2         # offset into rules/material_ext.mes file
 raceSpec.feats           = [feat_simple_weapon_proficiency_elf]
 
