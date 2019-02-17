0eaa4c6ae4d13b1a297460ca3d98635c4dccc159
Sebastian Hartte <sebastian@hartte.de>
2019-02-17 22:11:03 +0100

    Fixes memory corruption issue

--- TemplePlus/animgoals/anim.cpp
+++ TemplePlus/animgoals/anim.cpp
@@ -671,8 +671,6 @@ AnimSystem::AnimSystem(const GameSystemConf &config) {
   if (!startup(&config)) {
     throw TempleException("Unable to initialize game system Anim");
   }
-
-  animSysIsLoading = 0;
 }
 AnimSystem::~AnimSystem() {
   auto shutdown = temple::GetPointer<void()>(0x1000c110);
@@ -681,7 +679,6 @@ AnimSystem::~AnimSystem() {
 void AnimSystem::Reset() {
   auto reset = temple::GetPointer<void()>(0x1000c120);
   reset();
-  animSysIsLoading = 0;
 }
 bool AnimSystem::SaveGame(TioFile *file) {
   auto save = temple::GetPointer<int(TioFile *)>(0x1001cab0);
@@ -1360,7 +1357,7 @@ void AnimSystem::FreeSlot(AnimSlot & slot)
 	}
 
 	slot.Clear();
-	animAddresses.slotsInUse--;
+	(*animAddresses.slotsInUse)--;
 
 	if (!mActiveGoalCount) {
 		if (mAllGoalsClearedCallback) {
