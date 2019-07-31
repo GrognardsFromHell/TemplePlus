08d60c7db51fadafc6db924c0e26e91fea35ec7c
doug1234 <doug_ayers@yahoo.com>
2019-07-30 23:04:11 -0400

    Added code to fix an issue where a large number for charges was set then the feat is first added.

--- tpdatasrc/tpgamefiles/scr/tpModifiers/sudden_widen.py
+++ tpdatasrc/tpgamefiles/scr/tpModifiers/sudden_widen.py
@@ -52,6 +52,11 @@ def SuddenWidenDeductCharge(attachee, args, evt_obj):
 		
 	#Decriment the charges
 	charges = charges - 1
+	
+	#Prevent the issue where the charges start with a large value
+	if charges > 1000:
+		charges = 0
+	
 	args.set_arg(0, charges)
 
 	return 0
