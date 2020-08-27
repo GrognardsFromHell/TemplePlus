8bded7d2bbf57a5a3be08dea1a4b2e41ced8b78b
doug1234 <doug_ayers@yahoo.com>
2020-08-26 22:50:23 -0400

    Fix Rays of Enfeeblement (or Clumsiness) can't be empowered (or maximized) Atari Bug 170.

--- tpdatasrc/tpgamefiles/scr/Spell383 - Ray of Enfeeblement.py
+++ tpdatasrc/tpgamefiles/scr/Spell383 - Ray of Enfeeblement.py
@@ -1,4 +1,5 @@
 from toee import *
+import tpdp
 
 def OnBeginSpellCast( spell ):
 	print "Ray of Enfeeblement OnBeginSpellCast"
@@ -20,9 +21,33 @@ def OnBeginProjectile( spell, projectile, index_of_target ):
 def OnEndProjectile( spell, projectile, index_of_target ):
 	print "Ray of Enfeeblement OnEndProjectile"
 
+	spellPkt = tpdp.SpellPacket(spell.id)
+	mmData = tpdp.MetaMagicData(spellPkt.get_metamagic_data())
+
 	dice = dice_new( '1d6' )
-	dam_amount = dice.roll()
-	dam_amount += min( 5, spell.caster_level / 2 )
+	dam_bonus = min( 5, spell.caster_level / 2 )
+
+	if (mmData.get_maximize() and mmData.get_empower_count() > 0):
+		#SRD:  An empowered, maximized spell gains the separate benefits of each feat: the maximum result plus one-half the normally rolled result.
+		print "Maximized and Empower"
+		dam_amount = 6 + dam_bonus
+		dam_amount_emp = dice.roll()
+		dam_amount_emp += dam_bonus
+		dam_amount_emp /= 2
+		dam_amount += dam_amount_emp
+	elif (mmData.get_maximize()):
+		print "Maximized"
+		dam_amount = 6 + dam_bonus
+	elif (mmData.get_empower_count() > 0):
+		print "Empowered"
+		dam_amount = dice.roll()
+		dam_amount += dam_bonus
+		dam_amount_emp = dam_amount/2
+		dam_amount += dam_amount_emp
+	else:
+		dam_amount = dice.roll()
+		dam_amount += dam_bonus
+	
 	print "amount=", dam_amount
 
 	spell.duration = 10 * spell.caster_level
