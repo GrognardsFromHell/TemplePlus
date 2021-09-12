8d527c54f228c334ff0b1630facb7bf18ee5d8fb
DudeMcDude <Shai@DESKTOP-S05TA94>
2021-09-12 09:24:55 +0300

    Copy logfile when crashed so users don't have to do so manually

--- Infrastructure/breakpad.cpp
+++ Infrastructure/breakpad.cpp
@@ -4,11 +4,16 @@
 #include "infrastructure/breakpad.h"
 #include <fmt/format.h>
 #include "platform/windows.h"
+#include <infrastructure/stringutil.h>
+#include <filesystem>
+#include <Shlwapi.h>
+
+namespace fs = std::filesystem;
 
 Breakpad::Breakpad(const std::wstring &crashDumpFolder, bool fullDump)
 {
 
-	mHandler = std::make_unique<InProcessCrashReporting>(crashDumpFolder, fullDump, [this](const std::wstring &minidump_path) {
+	mHandler = std::make_unique<InProcessCrashReporting>(crashDumpFolder, fullDump, [this, crashDumpFolder](const std::wstring &minidump_path) {
 
 		auto msg = fmt::format(L"Sorry! TemplePlus seems to have crashed. A crash report was written to {}.\n\n"
 			L"If you want to report this issue, please contact us on our forums at RPGCodex or send an email to templeplushelp@gmail.com.",
@@ -20,6 +25,16 @@ Breakpad::Breakpad(const std::wstring &crashDumpFolder, bool fullDump)
 		// Now starts the tedious work of reporting on this crash, heh.
 		MessageBox(NULL, msg.c_str(), L"TemplePlus Crashed - Oops!", MB_OK);
 
+		// copy the log so it doesn't get erased on next startup
+		try {
+			std::wstring logfilename = crashDumpFolder + L"TemplePlus.log";
+			auto logfilenameNew = fs::path(minidump_path).replace_extension("log");
+			
+			std::filesystem::copy(logfilename, logfilenameNew);
+		}
+		catch (...) {
+
+		}
 	});
 
 }
