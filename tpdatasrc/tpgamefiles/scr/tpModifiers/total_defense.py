771b5562f01f2ea9185edde7363a6dda9a0ed6b0
doug1234 <doug_ayers@yahoo.com>
2021-09-15 22:25:37 -0400

    No AOO for total defense for issue #612.

--- tpdatasrc/tpgamefiles/scr/tpModifiers/total_defense.py
+++ tpdatasrc/tpgamefiles/scr/tpModifiers/total_defense.py
@@ -10,7 +10,13 @@ def FightingDefensivelyQuery(attachee, args, evt_obj):
 		evt_obj.return_val = 1
 		return 0
 	return 0
+	
+# No making AOOs when using total defense
+def TotalDefenseAOOPossible(attachee, args, evt_obj):
+	evt_obj.return_val = 0
+	return 0
 
 modExtender = PythonModifier()
 modExtender.ExtendExisting("Total Defense")
 modExtender.AddHook(ET_OnD20Query, EK_Q_FightingDefensively, FightingDefensivelyQuery, ())
+modExtender.AddHook(ET_OnD20Query, EK_Q_AOOPossible, TotalDefenseAOOPossible, ())
