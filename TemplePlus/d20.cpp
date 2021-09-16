393c65da2cee3e1f01ab0cbe3bd9b3b3d2da5138
doug1234 <doug_ayers@yahoo.com>
2021-09-15 21:38:53 -0400

    Fix spontaneous casters using sudden spells and abj camp extend ability for issue #614

--- TemplePlus/d20.cpp
+++ TemplePlus/d20.cpp
@@ -2491,13 +2491,16 @@ ActionErrorCode D20ActionCallbacks::ActionCostCastSpell(D20Actn * d20a, TurnBase
 	MetaMagicData mmData;
 	d20a->d20SpellData.Extract(&spEnum, nullptr, &spellClass, &spLvl, &invIdx, &mmData);
 
+	//For spontaneous casters don't use any changes made by metamagic modify when checking for full round cost
+	auto oldMMData = mmData;
+
 	//Modify metamagic information for quicken if necessary
 	dispatch.DispatchMetaMagicModify(d20Sys.globD20Action->d20APerformer, mmData);
 
 	SpellEntry spEntry(spEnum);
 
 	// Metamagicked spontaneous casters always cost full round to cast
-	if (mmData && !spellSys.isDomainSpell(spellClass) 
+	if (oldMMData && !spellSys.isDomainSpell(spellClass)
 		&& d20ClassSys.IsNaturalCastingClass(spellSys.GetCastingClass(spellClass))){
 		acp->hourglassCost = 4;
 		return AEC_OK;
