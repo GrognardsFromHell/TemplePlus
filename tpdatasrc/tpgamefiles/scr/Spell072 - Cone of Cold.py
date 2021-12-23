025770c10605e2f685b56d3dfb835614c5d14e67
Sagenlicht <herbstgeist@googlemail.com>
2021-12-23 19:00:40 +0100

    Fixed missing indicator files

--- tpdatasrc/tpgamefiles/scr/Spell072 - Cone of Cold.py
+++ tpdatasrc/tpgamefiles/scr/Spell072 - Cone of Cold.py
@@ -1,39 +1,38 @@
 from toee import *
 
 def OnBeginSpellCast( spell ):
-    print "Cone of Cold OnBeginSpellCast"
-    print "spell.target_list=", spell.target_list
-    print "spell.caster=", spell.caster, " caster.level= ", spell.caster_level
-    game.particles( "sp-evocation-conjure", spell.caster )
+	print "Cone of Cold OnBeginSpellCast"
+	print "spell.target_list=", spell.target_list
+	print "spell.caster=", spell.caster, " caster.level= ", spell.caster_level
+	game.particles( "sp-evocation-conjure", spell.caster )
 
-def OnSpellEffect( spell ):
-    print "Cone of Cold OnSpellEffect"
+def	OnSpellEffect( spell ):
+	print "Cone of Cold OnSpellEffect"
 
-    targetsToRemove = []
-    spell.duration = 0
-    saveType = D20_Save_Reflex
-    saveDescriptor = D20STD_F_NONE
-    spellDamageDice = dice_new('1d6')
-    spellDamageDice.number = min(spell.caster_level, 15)
-    damageType = D20DT_COLD
+	remove_list = []
 
-    game.particles( 'sp-Cone of Cold', spell.caster )
-    for spellTarget in spell.target_list:
-        #Saving throw for half damage
-        if spellTarget.obj.saving_throw_spell(spell.dc, saveType, saveDescriptor, spell.caster, spell.id): #success
-            spellTarget.obj.float_mesfile_line('mes\\spell.mes', 30001)
-            spellTarget.obj.spell_damage_with_reduction(spell.caster, damageType, spellDamageDice, D20DAP_UNSPECIFIED, DAMAGE_REDUCTION_HALF, D20A_CAST_SPELL, spell.id)
-        else:
-            spellTarget.obj.float_mesfile_line('mes\\spell.mes', 30002)
-            spellTarget.obj.spell_damage(spell.caster, damageType, spellDamageDice, D20DAP_UNSPECIFIED, D20A_CAST_SPELL, spell.id)
-        targetsToRemove.append(spellTarget.obj)
+	dam = dice_new( '1d6' )
+	dam.number = min( 15, spell.caster_level )
 
-    if targetsToRemove:
-        spell.target_list.remove_list(targetsToRemove)
-    spell.spell_end(spell.id)
+	game.particles( 'sp-Cone of Cold', spell.caster )
+
+	# get all targets in a 25ft + 2ft/level cone (60')
+	for target_item in spell.target_list:
+
+		if target_item.obj.reflex_save_and_damage( spell.caster, spell.dc, D20_Save_Reduction_Half, D20STD_F_NONE, dam, D20DT_COLD, D20DAP_UNSPECIFIED, D20A_CAST_SPELL, spell.id ) > 0:
+			# saving throw successful
+			target_item.obj.float_mesfile_line( 'mes\\spell.mes', 30001 )
+		else:
+			# saving throw unsuccessful
+			target_item.obj.float_mesfile_line( 'mes\\spell.mes', 30002 )
+
+		remove_list.append( target_item.obj )
+
+	spell.target_list.remove_list( remove_list )
+	spell.spell_end( spell.id )
 
 def OnBeginRound( spell ):
-    print "Cone of Cold OnBeginRound"
+	print "Cone of Cold OnBeginRound"
 
 def OnEndSpellCast( spell ):
-    print "Cone of Cold OnEndSpellCast"
+	print "Cone of Cold OnEndSpellCast"
