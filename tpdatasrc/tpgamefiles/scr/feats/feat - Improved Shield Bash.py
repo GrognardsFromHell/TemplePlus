4b4f4c909869f0f643680b1cd428608693389289
Sagenlicht <herbstgeist@googlemail.com>
2022-01-14 09:38:02 +0100

    Changed Shield Charge Prereq

--- tpdatasrc/tpgamefiles/scr/feats/feat - Improved Shield Bash.py
+++
@@ -1,7 +0,0 @@
-from toee import *
-import char_editor
-
-def CheckPrereq(attachee, classLevelled, abilityScoreRaised):
-    if char_editor.has_feat(feat_shield_proficiency):
-        return 1
-    return 0
