7094baf52582707471b23bfdf4e1dd4addc34743
Sagenlicht <herbstgeist@googlemail.com>
2022-03-21 10:32:35 +0100

    Added SF + GSF Invocation

---
+++ tpdatasrc/tpgamefiles/scr/tpModifiers/spell_focus_invocation.py
@@ -0,0 +1,18 @@
+from toee import *
+import tpdp
+from feat_utils import FeatPythonModifier
+from warlock import isInvocation
+
+print "Registering Spell Focus (Invocation)"
+
+def dcBonus(attachee, args, evt_obj):
+    spellEntry = evt_obj.spell_entry
+    if isInvocation(spellEntry.spell_enum):
+        bonusValue = 1
+        bonusType = bonus_type_untyped # Stacking!
+        bonusLabel = "~Spell Foucs (Invocation)~[TAG_SPELL_FOCUS]"
+        evt_obj.bonus_list.add(bonusValue, bonusType, bonusLabel)
+    return 0
+
+sfInvocationFeat = FeatPythonModifier("Spell Focus (Invocation)") #featEnum, empty
+sfInvocationFeat.AddHook(ET_OnGetSpellDcMod, EK_NONE, dcBonus, ())
