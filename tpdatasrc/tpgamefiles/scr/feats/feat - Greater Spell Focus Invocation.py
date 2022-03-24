9defe5c78497b9f6643a838eecbd4ce743fc9a89
Sagenlicht <herbstgeist@googlemail.com>
2022-03-24 10:56:37 +0100

    Warlock Updates
    GSF (Invocation) did not work with brackets, so I did rename it without brackets
    Started to add Eldritch Doom (WIP)

--- tpdatasrc/tpgamefiles/scr/feats/feat - Greater Spell Focus Invocation.py
+++ tpdatasrc/tpgamefiles/scr/feats/feat - Greater Spell Focus Invocation.py
@@ -2,6 +2,8 @@ from toee import *
 import char_editor
 
 def CheckPrereq(attachee, classLevelled, abilityScoreRaised):
+    print "CheckPrereq Hook for GSF Invocation"
     if not char_editor.has_feat("Spell Focus (Invocation)"):
+        print "Does not have feat requirements"
         return 0
     return 1
