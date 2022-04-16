43b3540eb7db8debdb72651cae451c121cb9abd3
doug1234 <doug_ayers@yahoo.com>
2022-04-15 22:14:13 -0400

    Added empty spell school check to SpellIsForbidden().

--- TemplePlus/ui/ui_char_editor.cpp
+++ TemplePlus/ui/ui_char_editor.cpp
@@ -3487,6 +3487,8 @@ bool Chargen::SpellIsForbidden(int spEnum, int spellClass){
 		SpellEntry spEntry(spEnum);
 		auto spSchool = spEntry.spellSchoolEnum;
 
+		if (spSchool == School_None) //Added for Invocations
+			return false;
 		if (spSchool == selPkt.forbiddenSchool1
 			|| spSchool == selPkt.forbiddenSchool2)
 			return true;
