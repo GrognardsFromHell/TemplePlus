66f32ba4e5f4a5afb14222d022d31dc4e16c2008
doug1234 <doug_ayers@yahoo.com>
2023-10-04 22:18:49 -0400

    Reworked touch attack attacks of opportunity.

--- tpdatasrc/tpgamefiles/scr/tpModifiers/karmic_strike.py
+++ tpdatasrc/tpgamefiles/scr/tpModifiers/karmic_strike.py
@@ -24,7 +24,7 @@ def KarmicStrikeTakingDamage(attachee, args, evt_obj):
 	
 	action_type = evt_obj.attack_packet.action_type
 	
-	if action_type == D20A_UNSPECIFIED_ATTACK or action_type == D20A_FULL_ATTACK or action_type == D20A_STANDARD_ATTACK:
+	if action_type == D20A_UNSPECIFIED_ATTACK or action_type == D20A_FULL_ATTACK or action_type == D20A_STANDARD_ATTACK or action_type == D20A_TOUCH_ATTACK:
 		opponent = evt_obj.attack_packet.attacker
 		attachee.make_aoo_if_possible(opponent)
 	
@@ -35,20 +35,9 @@ def KarmicStrikeACPenalty(attachee, args, evt_obj):
 		evt_obj.bonus_list.add(-4 , 0, "Karmic Strike") #Unnamed penalty
 	return 0
 	
-def KarmicStrikeTouchTarget(attachee, args, evt_obj):
-	if args.get_arg(0) <= 0:
-		return 0
-	
-	action = evt_obj.get_d20_action()
-	opponent = action.performer
-	attachee.make_aoo_if_possible(opponent)
-	
-	return 0
-
 #Setup the feat
 KarmicStrikeFeat = PythonModifier("Karmic Strike Feat", 2) # spare, spare
 KarmicStrikeFeat.MapToFeat("Karmic Strike")
-KarmicStrikeFeat.AddHook(ET_OnD20PythonSignal, "Touch Attack Victim", KarmicStrikeTouchTarget, ())
 KarmicStrikeFeat.AddHook(ET_OnBuildRadialMenuEntry, EK_NONE, KarmicStrikeRadial, ())
 KarmicStrikeFeat.AddHook(ET_OnTakingDamage2, EK_NONE, KarmicStrikeTakingDamage, ())
 KarmicStrikeFeat.AddHook(ET_OnGetAC, EK_NONE, KarmicStrikeACPenalty, ())
