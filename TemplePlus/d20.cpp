96fbf4fa781c18e4f27bb49cfefb0414525e4e30
Dan Doel <dan.doel@gmail.com>
2024-01-14 21:31:29 -0500

    Use toggle to choose attack anim

--- TemplePlus/d20.cpp
+++ TemplePlus/d20.cpp
@@ -1276,7 +1276,7 @@ ActionErrorCode D20ActionCallbacks::PerformStandardAttack(D20Actn* d20a)
 	if (d20Sys.UsingSecondaryWeapon(d20a))
 	{
 		d20a->d20Caf |= D20CAF_SECONDARY_WEAPON; 
-		useSecondaryAnim = 1;
+		useSecondaryAnim = !d20Sys.d20Query(DK_QUE_Left_Is_Primary);
 	}
 	else if (d20a->data1 >= ATTACK_CODE_NATURAL_ATTACK + 1)
 	{
