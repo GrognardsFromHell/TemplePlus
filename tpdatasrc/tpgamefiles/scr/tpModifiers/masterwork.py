4462dfe433a0fe32f3110d5bb27e7959649e9776
Dan Doel <dan.doel@gmail.com>
2025-02-18 02:49:40 -0500

    Add armor check adjustment for enhancement conditions
    Armor and shield enhancements mark items as masterwork, because crafting
    strips off the masterwork condition. The armor check reduction is
    guarded by checking for a spell id, though, because the same conditions
    are used for temporary buff spells which should not make the armor
    masterwork.
    
    Switched the bonus type of the adjustment to 12, 'enhancement bonus,' to
    prevent stacking in case an item ends up with multiple
    masterwork/enhancement conditions.

--- tpdatasrc/tpgamefiles/scr/tpModifiers/masterwork.py
+++ tpdatasrc/tpgamefiles/scr/tpModifiers/masterwork.py
@@ -12,8 +12,11 @@ def Bump(attachee, args, evt_obj):
 	# Apply bonus if item dispatch or armor is worn in the right
 	# slot. Null attachee means item dispatch.
 	if attachee == OBJ_HANDLE_NULL or verifyItem(armor, args):
+		# 12 is enhancement. Prevent stacking with enhancement conditions just
+		# in case.
+		#
 		# 242 is Masterwork
-		evt_obj.bonus_list.add(1, 0, 242)
+		evt_obj.bonus_list.add(1, 12, 242)
 
 	return 0
 
