e862886cfc0bf276c59f53f1b6e494cb5295fce7
Dan Doel <dan.doel@gmail.com>
2025-09-04 19:20:38 -0400

    Rewrite Co8 Holy Word
    Adds some features
    
    - Inflicts non-spell conditions for more appropriate effects
    - Checks for home planes based on in-game maps
    - Checks for silence/deafness for various effects
    - Banishment effect uses `critter_banish` for better visuals

--- tpdatasrc/co8infra/scr/Spell233 - Holy Word.py
+++ tpdatasrc/co8infra/scr/Spell233 - Holy Word.py
@@ -1,93 +1,110 @@
 from toee import *
 
-def OnBeginSpellCast( spell ):
-	print "Holy Word OnBeginSpellCast"
-	print "spell.target_list=", spell.target_list
-	print "spell.caster=", spell.caster, " caster.level= ", spell.caster_level
-	game.particles( "sp-evocation-conjure", spell.caster )
+air_node = 5081
+earth_node = 5082
+fire_node = 5083
+water_node = 5084
 
-def OnSpellEffect ( spell ):
-	print "Holy Word OnSpellEffect"
+nodes = set([air_node, earth_node, fire_node, water_node])
 
+def IsOnHomePlane(critter):
+	# nodes are created pocked planes with no natives
+	if critter.map in nodes: return False
+
+	# all other maps are (I think) prime material plane, so extraplanar flags
+	# apply, either monster subtype or npc flags.
+	is_extra_sub = critter.is_category_subtype(mc_subtype_extraplanar)
+	is_extra_flag = critter.npc_flags_get() & ONF_EXTRAPLANAR
+	return not (is_extra_sub or is_extra_flag)
+
+# checks if a creature can hear the holy word, because most effects occur only
+# to people that hear the word
+def CannotHear(critter):
+	in_silence = critter.d20_query_has_condition('sp-Silence Hit')
+	deaf = critter.d20_query(Q_Critter_Is_Deafened)
+	return in_silence or deaf
+
+def OnBeginSpellCast(spell):
+	return
+
+def OnSpellEffect(spell):
 	remove_list = []
+	ungood_list = []
 
-	# The Will save versus banishment is at a -4 penalty
-	spell.dc = spell.dc + 4
-	
-	
-	obj_list = []
-	# extracting the obj's so we don't get changed iterator bullshit (see http://www.co8.org/community/index.php?threads/holy-word-killed-my-cg-pc.12164/#post-145537)
+	# Copy valid targets to a separate list. Apparently prevents some oddities
+	# with the wrong targets being hit when the target list is used directly.
+	#
+	# http://www.co8.org/community/index.php?threads/holy-word-killed-my-cg-pc.12164/#post-145537
 	for target_item in spell.target_list:
-		# Only works on non-good creatures
-		target_item_obj = target_item.obj
-		alignment = target_item_obj.critter_get_alignment()
-		
-		if  not (alignment & ALIGNMENT_GOOD):
-			obj_list.append(target_item_obj )
-		remove_list.append( target_item_obj )
-	
-	#for target_item in spell.target_list:
-		#target_item_obj = target_item.obj
-	for target_item_obj in obj_list:
-	
-		print "target item: " + str(target_item_obj)
-		obj_hit_dice = target_item_obj.hit_dice_num
-						
-		game.particles( 'sp-Holy Smite', target_item_obj )
+		target = target_item.obj
 
-		# Anything ten or more levels below the caster's level dies
-		if obj_hit_dice <= (spell.caster_level - 10):
-			if not target_item_obj in game.leader.group_list():
-				#print "Inflicting damage due to low critter HD: " + str(target_item_obj)
-				target_item_obj.damage( game.leader , D20DT_POSITIVE_ENERGY, dice_new("52d52"))
-				# So you'll get awarded XP for the kill
-			#print "Killing due to low critter HD: " + str(target_item_obj)
-			target_item_obj.critter_kill()
+		alignment = target.critter_get_alignment()
 
-		# Anything five or more levels below the caster's level is paralyzed
-		if obj_hit_dice <= (spell.caster_level - 5):
-			spell.duration = game.random_range(1,10) * 10
-			target_item_obj.condition_add_with_args( 'sp-Hold Monster', spell.id, spell.duration, 0 )
+		if not (alignment & ALIGNMENT_GOOD):
+			ungood_list.append(target)
 
-		# Anything one or more levels below the caster's level is blinded
-		if obj_hit_dice <= (spell.caster_level - 1): 
-			spell.duration = game.random_range(1,4) + game.random_range(1,4)
-			target_item_obj.condition_add_with_args( 'sp-Blindness', spell.id, spell.duration, 0 )
+		# all targets will be removed at the end because Holy Word is an
+		# instantaneous spell
+		remove_list.append(target)
 
-		# Anything the caster's level or below is deafened
-		if obj_hit_dice <= (spell.caster_level):
-			spell.duration = game.random_range(1,4)
-			target_item_obj.condition_add_with_args( 'sp-Deafness', spell.id, spell.duration, 0 )
+	# If the caster is in an area of silence, the holy word is suppressed
+	if spell.caster.d20_query_has_condition('sp-Silence Hit'):
+		game.particles('Fizzle', spell.caster)
 
-			# Summoned and extraplanar creatures below the caster's level are also banished
-			# if they fail a Will save at -4
-			if target_item_obj.d20_query_has_spell_condition( sp_Summoned ) or target_item_obj.npc_flags_get() & ONF_EXTRAPLANAR != 0:
+		spell.target_list.remove_list(remove_list)
+		spell.spell_end(spell.id)
+		return
 
-				# allow Will saving throw to negate
-				if target_item_obj.saving_throw_spell( spell.dc, D20_Save_Will, D20STD_F_NONE, spell.caster, spell.id ):
+	# banishment effect only occurs on the caster's home plane
+	caster_in_home = IsOnHomePlane(spell.caster)
 
-					# saving throw successful
-					target_item_obj.float_mesfile_line( 'mes\\spell.mes', 30001 )
-					game.particles( 'Fizzle', target_item_obj )
-
+	for target in ungood_list:
+		if caster_in_home:
+			summoned = target.d20_query_has_spell_condition(sp_Summoned)
+			can_banish = summoned or not IsOnHomePlane(target)
+			if can_banish:
+				save = target.saving_throw_spell(
+					spell.dc + 4, D20_Save_Will, D20STD_F_NONE, spell.caster, spell.id)
+				if save:
+					target.float_mesfile_line('mes\\spell.mes', 30001)
 				else:
 					# saving throw unsuccessful
-					target_item_obj.float_mesfile_line( 'mes\\spell.mes', 30002 )
+					target.float_mesfile_line('mes\\spell.mes', 30002)
 
-					# creature is sent back to its own plane
-					# kill for now
-					if not target_item_obj in game.leader.group_list():
-						#print "Inflicting damage due to Summoned/Extraplanar: " + str(target_item_obj)
-						target_item_obj.damage( game.leader , D20DT_POSITIVE_ENERGY, dice_new("52d52"))
-						# So you'll get awarded XP for the kill
-						
-					#print "critter_kill due to Summoned or Extraplanar: " + str(target_item_obj)
-					target_item_obj.critter_kill()
-	spell.target_list.remove_list( remove_list )
+					game.particles('sp-Holy Smite', target)
+
+					target.obj_set_obj(obj_f_last_hit_by, spell.caster)
+					target.critter_banish(spell.caster)
+					# target is gone, no need for other conditions
+					continue
+
+		# The remaining effects are contingent on the target hearing the holy word.
+		if CannotHear(target): continue
+
+		game.particles('sp-Holy Smite', target)
+
+		# use non-spell conditions, because Holy Word is not an ongoing spell, it
+		# is an instantaneous spell that inflicts ongoing conditions.
+		if target.hit_dice_num <= spell.caster_level:
+			dur = dice_new('1d4').roll()
+			target.condition_add_with_args('Deafness', dur)
+
+		if target.hit_dice_num <= spell.caster_level-1:
+			dur = dice_new('2d4').roll()
+			target.condition_add_with_args('Blindness', dur)
+
+		if target.hit_dice_num <= spell.caster_level-5:
+			dur = 10 * dice_new('1d10').roll()
+			# store DC for Remove Paralysis
+			target.condition_add_with_args('Paralyzed', dur, spell.dc)
+
+		if target.hit_dice_num <= spell.caster_level-10:
+			target.obj_set_obj(obj_f_last_hit_by, spell.caster)
+			target.critter_kill()
+
+	spell.target_list.remove_list(remove_list)
 	spell.spell_end(spell.id)
 
-def OnBeginRound( spell ):
-	print "Holy Word OnBeginRound"
+def OnBeginRound(spell): return
 
-def OnEndSpellCast( spell ):
-	print "Holy Word OnEndSpellCast"
+def OnEndSpellCast(spell): return
